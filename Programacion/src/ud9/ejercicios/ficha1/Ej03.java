19d61a244b5d0405ac220c4ecdde36f3cb20a284
Diurno <Diurno@DAM124>
2026-03-18 13:27:27 +0100

    3,4 actualizados

--- Programacion/src/ud9/ejercicios/ficha1/Ej03.java
+++ Programacion/src/ud9/ejercicios/ficha1/Ej03.java
@@ -1,12 +1,26 @@
 package ud9.ejercicios.ficha1;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Ej03 {
 //	 Implementa un programa que cree un vector tipo double de tamaño 5 y luego,
 //	 utilizando un bucle, pida cinco valores por teclado y los introduzca en el vector.
 //	 Tendrás que manejar la/las posibles excepciones y seguir pidiendo valores
 //	 hasta rellenar completamente el vector
-	
-	double vector[] = new double[5];
-	
-	
+
+	public static void main(String[] args) {
+
+		double vector[] = new double[5];
+		Scanner leer= new Scanner(System.in);
+		
+		try {
+		for(int i = 0; i<5;i++) {
+			System.out.println("Introduce un valor ");
+			vector[i]=leer.nextDouble();
+		}
+		}catch(InputMismatchException e) {
+			System.out.println("Valor introducido incorrecto-");
+		}
+	}
 }
--- Programacion/src/ud9/ejercicios/ficha1/Ej04.java
+++ Programacion/src/ud9/ejercicios/ficha1/Ej04.java
@@ -1,5 +1,13 @@
 package ud9.ejercicios.ficha1;
 
 public class Ej04 {
-
+	public static void main(String[] args) {
+//		Implementa un programa que cree un vector de enteros de tamaño N (número
+//		aleatorio entre 1 y 100) con valores aleatorios entre 1 y 10. Luego se le
+//		preguntará al usuario qué posición del vector quiere mostrar por pantalla,
+//		repitiéndose una y otra vez hasta que se introduzca un valor negativo. Maneja
+//		todas las posibles excepciones.
+		
+		
+	}
 }
